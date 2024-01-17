710eae6bff6c039eb6f981508958653fc4d8c281
abhiruchin21 <abhiruchinaware@gmail.com>
2024-01-17 10:02:48 +0530

    my change

--- s3.tf
+++ s3.tf
@@ -1,10 +1,17 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
-  acl = var.acl
   
-   versioning {
-    enabled = var.versioning
-  }
   
   tags = var.tags
+}
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.my-s3-bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.my-s3-bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
